f963c2bb3c00efefcd17ffb532b87b0df62b40a6
anas bd <anastrying05@gmail.com>
2025-04-09 19:17:03 +0100

    upload image

--- src/app/admin/addproducts/page.tsx
+++ src/app/admin/addproducts/page.tsx
@@ -1,69 +1,119 @@
+'use client'
+
 import Sidebar from '@/components/Sidebar'
+import {supabase} from '@/utils/supabase'
 import Image from 'next/image'
-import React from 'react'
+import React,{useState} from 'react'
+import {toast} from 'react-toastify'
 
 const AddPage = () => {
+    const [imageUrls,setImageUrls] = useState<(string | null)[]>([null,null,null,null])
+
+    const handleUpload = async (
+        e: React.ChangeEvent<HTMLInputElement>,
+        index: number
+    ) => {
+        const file = e.target.files?.[0]
+        if(!file) return
+
+        const filePath = `public/${file.name}`
+
+        const {data,error} = await supabase.storage
+            .from('shoeimages')
+            .upload(filePath,file)
+
+        if(error) {
+            console.error('Upload Error:',error)
+            toast.error("There is an image has been already uploaded" + error.message)
+        } else {
+            const {data: urlData} = supabase.storage
+                .from('shoeimages')
+                .getPublicUrl(filePath)
+
+            const newUrls = [...imageUrls]
+            newUrls[index] = urlData.publicUrl
+            setImageUrls(newUrls)
+        }
+    }
+
     return (
         <section className='w-full h-screen flex gap-5'>
             <Sidebar />
             <div className="w-5/6 ml-auto p-5">
                 <h1 className='text-3xl font-header font-bold'>Add Products</h1>
                 <main className='flex gap-4 flex-col w-3/5 my-8'>
+                    {/* Upload Images */}
                     <div className="flex flex-col">
                         <label className="text-gray-700">Upload Image</label>
                         <div className="flex gap-2">
-                            <div>
-                                <label htmlFor="img1">
-                                    <Image src="/upload.png" width={100} height={100} alt="" className="cursor-pointer" />
-                                </label>
-                                <input type="file" id="img1" hidden />
-                            </div>
-                            <div>
-                                <label htmlFor="img2">
-                                    <Image src="/upload.png" width={100} height={100} alt="" className="cursor-pointer" />
-                                </label>
-                                <input type="file" id="img2" hidden />
-                            </div>
-                            <div>
-                                <label htmlFor="img3">
-                                    <Image src="/upload.png" width={100} height={100} alt="" className="cursor-pointer" />
-                                </label>
-                                <input type="file" id="img3" hidden />
-                            </div>
-                            <div>
-                                <label htmlFor="img4">
-                                    <Image src="/upload.png" width={100} height={100} alt="" className="cursor-pointer" />
-                                </label>
-                                <input type="file" id="img4" hidden />
-                            </div>
+                            {[0,1,2,3].map((i) => (
+                                <div key={i}>
+                                    <label htmlFor={`img${i}`}>
+                                        <Image
+                                            src={imageUrls[i] || "/upload.png"}
+                                            width={100}
+                                            height={100}
+                                            alt="Upload preview"
+                                            className="cursor-pointer object-cover border rounded"
+                                        />
+                                    </label>
+                                    <input
+                                        onChange={(e) => handleUpload(e,i)}
+                                        type="file"
+                                        id={`img${i}`}
+                                        hidden
+                                    />
+                                </div>
+                            ))}
                         </div>
                     </div>
 
+                    {/* Shoe Name */}
                     <div className="flex flex-col">
-                        <label className='text-gray-700' >Shoe name</label>
-                        <input placeholder='Type Here!' className='py-1.5 px-3 border border-gray-400 rounded-sm' type="text" name="" id="" />
+                        <label className='text-gray-700'>Shoe name</label>
+                        <input
+                            placeholder='Type Here!'
+                            className='py-1.5 px-3 border border-gray-400 rounded-sm'
+                            type="text"
+                        />
                     </div>
+
+                    {/* Shoe Description */}
                     <div className="flex flex-col">
-                        <label className='text-gray-700' >Shoe description</label>
-                        <input placeholder='Type Here!' className='py-1.5 px-3 border border-gray-400 rounded-sm' type="text" />
+                        <label className='text-gray-700'>Shoe description</label>
+                        <input
+                            placeholder='Type Here!'
+                            className='py-1.5 px-3 border border-gray-400 rounded-sm'
+                            type="text"
+                        />
                     </div>
+
+                    {/* Shoe Price */}
                     <div className="flex flex-col w-1/4">
-                        <label className='text-gray-700' >Shoe Price</label>
-                        <input placeholder='Type Here!' className='py-1.5 px-3 border border-gray-400 rounded-sm' type="number" />
+                        <label className='text-gray-700'>Shoe Price</label>
+                        <input
+                            placeholder='Type Here!'
+                            className='py-1.5 px-3 border border-gray-400 rounded-sm'
+                            type="number"
+                        />
                     </div>
+
+                    {/* Shoe Sizes */}
                     <div className="flex-col flex mt-3">
-                        <label className='text-gray-700' >Shoe sizes</label>
+                        <label className='text-gray-700'>Shoe sizes</label>
                         <div className="flex gap-2">
-                            <button className='bg-black h-10 w-10 text-white'>38</button>
-                            <button className='bg-black h-10 w-10 text-white'>39</button>
-                            <button className='bg-black h-10 w-10 text-white'>40</button>
-                            <button className='bg-black h-10 w-10 text-white'>41</button>
-                            <button className='bg-black h-10 w-10 text-white'>42</button>
-                            <button className='bg-black h-10 w-10 text-white'>43</button>
-                            <button className='bg-black h-10 w-10 text-white'>44</button>
+                            {[38,39,40,41,42,43,44].map(size => (
+                                <button key={size} className='bg-black h-10 w-10 text-white'>
+                                    {size}
+                                </button>
+                            ))}
                         </div>
                     </div>
-                    <button className='bg-accent text-white py-2 w-32 rounded-sm'>Add Now</button>
+
+                    {/* Submit Button */}
+                    <button className='bg-accent text-white py-2 w-32 rounded-sm'>
+                        Add Now
+                    </button>
                 </main>
             </div>
         </section>
