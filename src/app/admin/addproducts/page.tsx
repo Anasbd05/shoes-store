b093af0df04d9c97a732c644bcf4cc96080486be
anas bd <anastrying05@gmail.com>
2025-04-09 15:43:35 +0100

    sort by price

--- src/app/admin/addproducts/page.tsx
+++ src/app/admin/addproducts/page.tsx
@@ -1,4 +1,5 @@
 import Sidebar from '@/components/Sidebar'
+import Image from 'next/image'
 import React from 'react'
 
 const AddPage = () => {
@@ -8,6 +9,36 @@ const AddPage = () => {
             <div className="w-5/6 ml-auto p-5">
                 <h1 className='text-3xl font-header font-bold'>Add Products</h1>
                 <main className='flex gap-4 flex-col w-3/5 my-8'>
+                    <div className="flex flex-col">
+                        <label className="text-gray-700">Upload Image</label>
+                        <div className="flex gap-2">
+                            <div>
+                                <label htmlFor="img1">
+                                    <Image src="/upload.png" width={100} height={100} alt="" className="cursor-pointer" />
+                                </label>
+                                <input type="file" id="img1" hidden />
+                            </div>
+                            <div>
+                                <label htmlFor="img2">
+                                    <Image src="/upload.png" width={100} height={100} alt="" className="cursor-pointer" />
+                                </label>
+                                <input type="file" id="img2" hidden />
+                            </div>
+                            <div>
+                                <label htmlFor="img3">
+                                    <Image src="/upload.png" width={100} height={100} alt="" className="cursor-pointer" />
+                                </label>
+                                <input type="file" id="img3" hidden />
+                            </div>
+                            <div>
+                                <label htmlFor="img4">
+                                    <Image src="/upload.png" width={100} height={100} alt="" className="cursor-pointer" />
+                                </label>
+                                <input type="file" id="img4" hidden />
+                            </div>
+                        </div>
+                    </div>
+
                     <div className="flex flex-col">
                         <label className='text-gray-700' >Shoe name</label>
                         <input placeholder='Type Here!' className='py-1.5 px-3 border border-gray-400 rounded-sm' type="text" name="" id="" />
@@ -23,13 +54,13 @@ const AddPage = () => {
                     <div className="flex-col flex mt-3">
                         <label className='text-gray-700' >Shoe sizes</label>
                         <div className="flex gap-2">
-                            <button className='bg-black h-8 w-8 text-white'>38</button>
-                            <button className='bg-black h-8 w-8 text-white'>39</button>
-                            <button className='bg-black h-8 w-8 text-white'>40</button>
-                            <button className='bg-black h-8 w-8 text-white'>41</button>
-                            <button className='bg-black h-8 w-8 text-white'>42</button>
-                            <button className='bg-black h-8 w-8 text-white'>43</button>
-                            <button className='bg-black h-8 w-8 text-white'>44</button>
+                            <button className='bg-black h-10 w-10 text-white'>38</button>
+                            <button className='bg-black h-10 w-10 text-white'>39</button>
+                            <button className='bg-black h-10 w-10 text-white'>40</button>
+                            <button className='bg-black h-10 w-10 text-white'>41</button>
+                            <button className='bg-black h-10 w-10 text-white'>42</button>
+                            <button className='bg-black h-10 w-10 text-white'>43</button>
+                            <button className='bg-black h-10 w-10 text-white'>44</button>
                         </div>
                     </div>
                     <button className='bg-accent text-white py-2 w-32 rounded-sm'>Add Now</button>
--- src/app/shoes/page.tsx
+++ src/app/shoes/page.tsx
@@ -1,37 +1,64 @@
 "use client"
-import React,{useState} from 'react'
+import React,{useEffect,useState} from 'react'
 import {shoes} from "../../assets/assets"
 import Image from 'next/image'
 import Link from 'next/link'
 
 const ShoesPage = () => {
+    const [shoeSearch,setShoeSearch] = useState("")
+    const [sortPrice,setSortPrice] = useState([...shoes])
 
-    const [shoeSearch,setshoeSearch] = useState("")
+    useEffect(() => {
+        setSortPrice([...shoes].sort((a,b) => a.price - b.price))
+    },[])
+
+    const sortShoeByPrice = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const value = e.target.value
+        const sorted = [...shoes].sort((a,b) =>
+            value === "low" ? a.price - b.price : b.price - a.price
+        )
+        setSortPrice(sorted)
+    }
 
     return (
         <section className='w-full px-1 sm:px-4 my-10'>
-            <div className="flex flex-col gap-4 md:flex-row w-full justify-between ">
+            <div className="flex flex-col gap-4 md:flex-row w-full justify-between">
                 <h1 className='text-3xl font-bold font-header'>Explore our shoes</h1>
-                <input type="text" value={shoeSearch} onChange={(e) => setshoeSearch(e.target.value)} placeholder='Search...' autoFocus className='w-full md:w-2/5 outline-none ring-1 ring-accent py-1 rounded-lg px-4' />
+                <div className="flex items-center gap-1">
+                    <p>Price :</p>
+                    <select className='bg-gray-900 text-white py-1 px-4 outline-none rounded-sm'
+                        onChange={sortShoeByPrice}>
+                        <option value="low">Low To High</option>
+                        <option value="high">High To Low</option>
+                    </select>
+                </div>
+                <input
+                    type="text"
+                    value={shoeSearch}
+                    onChange={(e) => setShoeSearch(e.target.value)}
+                    placeholder='Search...'
+                    autoFocus
+                    className='w-full md:w-2/5 outline-none ring-1 ring-accent py-1 rounded-lg px-4'
+                />
             </div>
+
             <main className='grid my-12 gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
                 {
-                    shoes.filter(
+                    sortPrice.filter(
                         (item) =>
                             item.title.toLowerCase().includes(shoeSearch.toLowerCase()) ||
                             item.description.toLowerCase().includes(shoeSearch.toLowerCase())
-                    )
-                        .map((shoe) => (
-                            <div key={shoe.id} className="flex justify-between bg-white shadow-md rounded-lg flex-col">
-                                <Image alt='' className='h-52 bg-gray-100  brightness-90 rounded-t-lg' src={shoe.image} />
-                                <div className="flex w-full px-2 py-4 justify-between">
-                                    <h1 className='sm:text-lg font-semibold font-header'>{shoe.title}</h1>
-                                    <h3 className='font-bold font-mono text-accent sm:text-lg'>{shoe.price} DH</h3>
-                                </div>
-                                <p className='mb-2 px-2 text-justify'>{shoe.description}</p>
-                                <Link href={`/shoes/${shoe.title.replaceAll(" ","")}`} className='py-1.5 mb-1 text-center font-medium hover:opacity-80 bg-accent rounded-md'>View</Link>
+                    ).map((shoe) => (
+                        <div key={shoe.id} className="flex justify-between bg-white shadow-md rounded-lg flex-col">
+                            <Image alt={shoe.title} className='h-52 bg-gray-100 brightness-90 rounded-t-lg' src={shoe.image} />
+                            <div className="flex w-full px-2 py-4 justify-between">
+                                <h1 className='sm:text-lg font-semibold font-header'>{shoe.title}</h1>
+                                <h3 className='font-bold font-mono text-accent sm:text-lg'>{shoe.price} DH</h3>
                             </div>
-                        ))
+                            <p className='mb-2 px-2 text-justify'>{shoe.description}</p>
+                            <Link href={`/shoes/${shoe.title.replaceAll(" ","")}`} className='py-1.5 mb-1 text-center font-medium hover:opacity-80 bg-accent rounded-md'>View</Link>
+                        </div>
+                    ))
                 }
             </main>
         </section>
